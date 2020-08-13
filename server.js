fee0d5783452b28f33ef46e12f8304545bda9d9f
pfzoso55 <58383054+pfzoso55@users.noreply.github.com>
2020-08-13 18:24:47 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var os = require("os");
+var hostname = os.hostname(); 
+var http = require('http');
+
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Hello World!\n' + hostname);
+};
+
+var www = http.createServer(handleRequest);
+
+www.listen(8082);
